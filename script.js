71604f01ce2b8c3b47454ee1afc25a5b7d178a43
Sandwichzaza <sandwith1g@gmail.com>
2025-04-11 20:10:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Ryu is chubby ! 😘";
+  question.innerHTML = "ไม่มีใครน่ารักเท่าปุยนุ่นแล้วบนโลกใบนี้😘";
   gif.src =
     "https://media1.giphy.com/media/iCVzZwwE6QNAV2tEE0/giphy.gif";
 });
